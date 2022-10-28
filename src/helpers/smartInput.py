907fcc0cb1c5d949e3785e2dd43e89e8e4a0affb
Michał Madeja <35845468+michal7954@users.noreply.github.com>
2022-10-28 16:17:05 +0200

    Niedziałająca strzałka w górę

--- src/helpers/smartInput.py
+++ src/helpers/smartInput.py
@@ -1,9 +1,15 @@
 import readchar
 from pyautogui import typewrite
+from pynput.keyboard import Key, Controller
+
+keyboard = Controller()
 
 def smartInput(operators):
     key = readchar.readkey()
-    if key in operators:
+    if key == '\033[A':
+        keyboard.press(Key.up)
+        key = input()
+    elif key in operators:
         print(key)
     elif key:
         typewrite(key)
