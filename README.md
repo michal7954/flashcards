189e87608a475254f3131096b933cbccbccb3666
Michał Madeja <35845468+michal7954@users.noreply.github.com>
2022-10-30 09:44:28 +0100

    Komunikat o zapauzowaniu timera

--- README.md
+++ README.md
@@ -3,4 +3,3 @@ Aplikacja do wielokrotnego powtarzania słówek.
 
 todo:
 ? widok początkowy - wykres ile słówek jest zaplanowanych na najbliższe dni
-Komunikat o zapauzowaniu timera
--- src/helpers/timer.py
+++ src/helpers/timer.py
@@ -7,36 +7,46 @@ from helpers.consts import maxInactivityTime
 timer = datetime(1, 1, 1, 0, 0, 0)
 inactivityTime = 0
 stopped = False
+active = True
 
 
 def resetInactivityTimer():
-    global inactivityTime
+    global inactivityTime, active
     inactivityTime = 0
+    active = True
+
 
 def getTimer():
     global timer
     return f'{timer.minute}:{timer.strftime("%S")}'
 
+
 def incrementTimer():
-    global inactivityTime, timer
-    inactivityTime+=1
+    global inactivityTime, timer, active
+    inactivityTime += 1
     if inactivityTime <= maxInactivityTime:
-        timer+=timedelta(0, 1)
+        timer += timedelta(0, 1)
+    else:
+        if active:
+            active = False
+            print("Zatrzymano odliczanie czasu\n")
+
 
 def stopTimer():
     global stopped
     stopped = True
 
-def setTimer(func, sec):
 
+def setTimer(func, sec):
     def func_wrapper():
-        if stopped: 
+        if stopped:
             return None
         setTimer(func, sec)
         func()
-        
+
     t = threading.Timer(sec, func_wrapper)
     t.start()
     return t
+
 
 setTimer(incrementTimer, 1)
